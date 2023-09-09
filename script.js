cef4d17bcf0d23332705127acea1eda3b7a1690f
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-09 14:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+function removeArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '');
+}
+
+touristSpots = touristSpots.map(removeArticles).sort();
+
+const ulElement = document.createElement('ul');
+ulElement.id = 'band';
+
+
+for (const spot of touristSpots) {
+  const liElement = document.createElement('li');
+  liElement.textContent = spot;
+  ulElement.appendChild(liElement);
+}
+
+document.body.appendChild(ulElement);
 
